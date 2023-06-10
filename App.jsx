425be4e43d50c59bed59752723c1fc638466d53b
Niyi0904 <125608524+Niyi0904@users.noreply.github.com>
2023-06-10 11:22:52 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './Components/NavDiv/NavBar'
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ Components/NavDiv/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const NavBar = () => {
+    return (
+        <div className='navBar flex justify-between items-center p-[3rem]'>
+            <div className="logoDiv">
+                <h1 className="logo text-[50px] text-blueColor">Niyi</h1>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
